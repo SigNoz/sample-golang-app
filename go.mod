e4fbe56ae257b31c4fa38b9b2ca3d5456ea00a45
nityanandagohain <nityanandagohain@gmail.com>
2022-04-30 17:58:05 +0530

    otelgorm added

--- go.mod
+++ go.mod
@@ -4,14 +4,15 @@ go 1.14
 
 require (
 	github.com/gin-gonic/gin v1.7.7
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-cmp v0.5.6 // indirect
-	github.com/jinzhu/gorm v1.9.12
-	github.com/kr/pretty v0.1.0 // indirect
-	github.com/lib/pq v1.2.0 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
+	github.com/mattn/go-sqlite3 v2.0.1+incompatible // indirect
+	github.com/uptrace/opentelemetry-go-extra/otelgorm v0.1.12
+	go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin v0.31.0
+	go.opentelemetry.io/otel v1.6.3
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.6.3
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.6.3
+	go.opentelemetry.io/otel/sdk v1.6.3
+	google.golang.org/grpc v1.46.0
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gorm.io/driver/sqlite v1.3.2
+	gorm.io/gorm v1.23.5
 )
--- main.go
+++ main.go
@@ -1,14 +1,85 @@
 package main
 
 import (
+	"context"
+	"log"
+	"os"
+
 	"github.com/SigNoz/sample-golang-app/controllers"
 	"github.com/SigNoz/sample-golang-app/models"
+	"google.golang.org/grpc/credentials"
 
 	"github.com/gin-gonic/gin"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
+var (
+	serviceName  = os.Getenv("SERVICE_NAME")
+	signozToken  = os.Getenv("SIGNOZ_ACCESS_TOKEN")
+	collectorURL = os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT")
+	insecure     = os.Getenv("INSECURE_MODE")
+)
+
+func initTracer() func(context.Context) error {
+
+	headers := map[string]string{
+		"signoz-access-token": signozToken,
+	}
+
+	secureOption := otlptracegrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, ""))
+	if len(insecure) > 0 {
+		secureOption = otlptracegrpc.WithInsecure()
+	}
+
+	exporter, err := otlptrace.New(
+		context.Background(),
+		otlptracegrpc.NewClient(
+			secureOption,
+			otlptracegrpc.WithEndpoint(collectorURL),
+			otlptracegrpc.WithHeaders(headers),
+		),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	resources, err := resource.New(
+		context.Background(),
+		resource.WithAttributes(
+			attribute.String("service.name", serviceName),
+			attribute.String("library.language", "go"),
+		),
+	)
+	if err != nil {
+		log.Printf("Could not set resources: ", err)
+	}
+
+	otel.SetTracerProvider(
+		sdktrace.NewTracerProvider(
+			sdktrace.WithSampler(sdktrace.AlwaysSample()),
+			sdktrace.WithSpanProcessor(sdktrace.NewBatchSpanProcessor(exporter)),
+			sdktrace.WithSyncer(exporter),
+			sdktrace.WithResource(resources),
+		),
+	)
+	return exporter.Shutdown
+}
+
 func main() {
+
+	cleanup := initTracer()
+	defer cleanup(context.Background())
+
 	r := gin.Default()
+	r.Use(otelgin.Middleware(serviceName))
+	// Connect to database
 	models.ConnectDatabase()
 
 	// Routes
